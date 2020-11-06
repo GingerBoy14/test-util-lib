5c06d64862f84f216d86792db9cda57e6c2e4f1c
GingerBoy14 <maks.27.04.2002@gmail.com>
2020-11-06 11:36:20 +0200

    use showLog

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,18 @@
 describe('first test', () => {
   it('first iteration', () => {
+    expect(9).toBe(8)
+  })
+})
+describe('second second test', () => {
+  it('first iteration', () => {
     expect(9).toBe(9)
+  })
+  it('second iteration', () => {
+    expect(9).toBe(19)
+  })
+})
+describe('third test', () => {
+  it('first iteration', () => {
+    expect(9).toBe(8)
   })
 })
