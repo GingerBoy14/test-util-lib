6264f8dd832d424733b8fa632d31a7d9abe0bb72
GingerBoy14 <maks.27.04.2002@gmail.com>
2020-11-09 15:12:51 +0200

    fix error <h> in <p>

--- src/componetns/FileNotFound/FileNotFound.js
+++ src/componetns/FileNotFound/FileNotFound.js
@@ -1,14 +1,16 @@
 import { Row, Col, Typography, Button, Empty } from 'antd'
 
 import EmptyLog from './empty.svg'
-const { Title } = Typography
+const { Text } = Typography
 
 const FileNotFound = ({ fetchData }) => (
   <Row justify="center" align="middle" style={{ height: '100%' }}>
     <Col xs={24} sm={20} md={18} lg={10} xl={8} xxl={6} justify="center">
       <Empty
         image={<img src={EmptyLog} alt="Empty" style={{ width: '100%' }} />}
-        description={<Title level={4}>Can't find logs file</Title>}
+        description={
+          <Text style={{ fontSize: '1.5rem' }}>Can't find logs file</Text>
+        }
         imageStyle={{ height: 'auto' }}>
         <Button type="secondary" onClick={fetchData}>
           Run tests
